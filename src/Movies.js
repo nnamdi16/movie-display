281b4f25c4843d6b0b31932ca1e98c14166ab7a0
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-03 18:56:04 +0100

    Add react-overdrive for animation

--- src/Movies.js
+++ src/Movies.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
 
 export const Poster = styled.img`box-shadow: 0 0 35px black;`;
 
@@ -35,7 +36,9 @@ export const Poster = styled.img`box-shadow: 0 0 35px black;`;
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 const Movie = ({ movie }) => (
 	<Link to={`${movie.id}`}>
-		<Poster src={`${POSTER_PATH}${movie.poster_path}`} alt="{movie.title}" />
+		<Overdrive id={movie.id}>
+			<Poster src={`${POSTER_PATH}${movie.poster_path}`} alt="{movie.title}" />
+		</Overdrive>
 	</Link>
 );
 
