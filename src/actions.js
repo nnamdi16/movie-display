a5c7dd7ad114f0fc492884537b8db6d4a0f40391
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-04 22:13:54 +0100

    create an action object

---
+++ src/actions.js
@@ -0,0 +1,11 @@
+export const TOGGLE_MESSAGE = 'TOGGLE_MESSAGE';
+
+export function toggleMessage() {
+	return {
+		type: 'TOGGLE_MESSAGE'
+	};
+}
+
+// export function getMovies() {
+// 	return async function(params) {};
+// }
