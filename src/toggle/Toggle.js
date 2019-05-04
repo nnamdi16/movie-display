e9afff5785b38eec141d54ee99652c177ad8d28c
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-05 00:08:58 +0100

    Add actions from toggle and movies action creators with their respective reducer aand actions in mapStateToProps and mapDispatchToProps

--- src/toggle/Toggle.js
+++ src/toggle/Toggle.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { toggleMessage } from './actions';
 import { bindActionCreators } from 'redux';
-const Toggle = ({ messageVisibility, toggleMessage, getMovies }) => (
+import { toggleMessage } from './actions';
+import { getMovies } from '../movies/actions';
+const Toggle = ({ messageVisibility, toggleMessage }) => (
 	<div>
 		{messageVisibility && <p>You will be seeing this if redux is toggled</p>}
 		<button onClick={toggleMessage}>Toggle Me</button>
-		<button onClick={getMovies}>Load Movies</button>
+		{/* <button onClick={getMovies}>Load Movies</button> */}
 	</div>
 );
 
 const mapStateToProps = state => {
 	return {
-		messageVisibility: state.message.messageVisibility
+		messageVisibility: state.toggle.messageVisibility
 	};
 };
 
