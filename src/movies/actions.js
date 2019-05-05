11283a6ee82d17c843ac3005f7829cde6ead4ba0
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-05 11:33:24 +0100

    Create a getMovie and resetMovie action

--- src/movies/actions.js
+++ src/movies/actions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 export const GET_MOVIES = 'GET_MOVIES';
+export const GET_MOVIE = 'GET_MOVIE';
+export const RESET_MOVIE = 'RESET_MOVIE';
 
 export function getMovies() {
 	return async function(dispatch) {
@@ -12,5 +14,25 @@ export function getMovies() {
 			type: 'GET_MOVIES',
 			data: movies.results
 		});
+	};
+}
+
+export function getMovie(id) {
+	return async function(dispatch) {
+		const response = await axios.get(
+			`https://api.themoviedb.org/3/movie/${id}?api_key=6274c1c8045c07d02c232749ff8562ba&language=en-US`
+		);
+		console.log(response);
+		const movie = response.data;
+		return dispatch({
+			type: 'GET_MOVIE',
+			data: movie
+		});
+	};
+}
+
+export function resetMovie() {
+	return {
+		type: 'RESET_MOVIE'
 	};
 }
